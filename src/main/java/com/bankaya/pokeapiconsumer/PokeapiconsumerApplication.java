607ada8771350c2353a12674c3c8a114f79ef1e4
Rair <rair.guzman15@gmail.com>
2024-07-06 12:54:22 -0600

    initial setup

---
+++ src/main/java/com/bankaya/pokeapiconsumer/PokeapiconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.bankaya.pokeapiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeapiconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeapiconsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankaya/pokeapiconsumer/PokeapiconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankaya.pokeapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapiconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
