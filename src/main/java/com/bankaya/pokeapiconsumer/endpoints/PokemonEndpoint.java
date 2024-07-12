f2114fdf5c6d45a485b1a643a9ad10f14dbeccf0
Rair <rair.guzman15@gmail.com>
2024-07-12 14:30:55 -0600

    added duration, request and response to log as events

--- src/main/java/com/bankaya/pokeapiconsumer/endpoints/PokemonEndpoint.java
+++ src/main/java/com/bankaya/pokeapiconsumer/endpoints/PokemonEndpoint.java
@@ -5,16 +5,29 @@ import com.bankaya.pokeapiconsumer.models.Event;
 import com.bankaya.pokeapiconsumer.repositories.EventRepository;
 import com.bankaya.pokeapiconsumer.services.PokemonService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import java.io.StringWriter;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.logging.Logger;
 
 @Endpoint
 public class PokemonEndpoint {
+    Logger logger = Logger.getLogger(getClass().getName());
     private static final String NAMESPACE_URI = "http://www.bankaya.com/pokeapiconsumer";
+    private StringWriter sw;
+    private String xmlRequest;
+    private String xmlResponse;
+    private Marshaller jaxbMarshaller;
+    private JAXBContext contextObj;
     private final HttpServletRequest httpServletRequest;
     private final PokemonService pokemonService;
     private final EventRepository eventRepository;
@@ -25,59 +38,228 @@ public class PokemonEndpoint {
         this.eventRepository = eventRepository;
     }
 
+    private void xmlContext() throws JAXBException {
+        jaxbMarshaller = contextObj.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    }
+
+    private void logEvent(String ipAddress, LocalDateTime dateTime, Long duration, String request, String response) {
+        String methodName = StackWalker.getInstance()
+                .walk(s -> s.skip(1).findFirst())
+                .get()
+                .getMethodName();
+
+        eventRepository.save(new Event(
+                ipAddress,
+                dateTime,
+                methodName,
+                duration,
+                request,
+                response
+        ));
+    }
+
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "pokemonNameRequest")
     @ResponsePayload
     public PokemonNameResponse getPokemonName(@RequestPayload PokemonNameRequest request) {
-        logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), "getPokemonName");
+        Instant start = Instant.now();
 
-        return pokemonService.getPokemonName(request.getId());
+        try {
+            //generation of xml with request to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(request.getClass());
+            xmlContext();
+            jaxbMarshaller.marshal(request, sw);
+            xmlRequest = sw.toString();
+            xmlRequest = xmlRequest.replace("\n", "");
+
+            PokemonNameResponse response = pokemonService.getPokemonName(request.getId());
+
+            //generation of xml with response to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(response.getClass());
+            jaxbMarshaller = contextObj.createMarshaller();
+            jaxbMarshaller.marshal(response, sw);
+            xmlResponse = sw.toString();
+
+            return response;
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //logging event
+            Instant finish = Instant.now();
+            long duration = Duration.between(start, finish).toMillis();
+            logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), duration, xmlRequest, xmlResponse);
+        }
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "pokemonIdRequest")
     @ResponsePayload
     public PokemonIdResponse getPokemonId(@RequestPayload PokemonIdRequest request) {
-        logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), "getPokemonId");
+        Instant start = Instant.now();
 
-        return pokemonService.getPokemonId(request.getName());
+        try {
+            //generation of xml with request to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(request.getClass());
+            xmlContext();
+            jaxbMarshaller.marshal(request, sw);
+            xmlRequest = sw.toString();
+            xmlRequest = xmlRequest.replace("\n", "");
+
+            PokemonIdResponse response = pokemonService.getPokemonId(request.getName());
+
+            //generation of xml with response to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(response.getClass());
+            jaxbMarshaller = contextObj.createMarshaller();
+            jaxbMarshaller.marshal(response, sw);
+            xmlResponse = sw.toString();
+
+            return response;
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //logging event
+            Instant finish = Instant.now();
+            long duration = Duration.between(start, finish).toMillis();
+            logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), duration, xmlRequest, xmlResponse);
+        }
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "pokemonBaseExperienceRequest")
     @ResponsePayload
     public PokemonBaseExperienceResponse getPokemonBaseExperience(@RequestPayload PokemonBaseExperienceRequest request) {
-        logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), "getPokemonBaseExperience");
+        Instant start = Instant.now();
 
-        return pokemonService.getPokemonBaseExperience(request.getName());
+        try {
+            //generation of xml with request to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(request.getClass());
+            xmlContext();
+            jaxbMarshaller.marshal(request, sw);
+            xmlRequest = sw.toString();
+            xmlRequest = xmlRequest.replace("\n", "");
+
+            PokemonBaseExperienceResponse response = pokemonService.getPokemonBaseExperience(request.getName());
+
+            //generation of xml with response to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(response.getClass());
+            jaxbMarshaller = contextObj.createMarshaller();
+            jaxbMarshaller.marshal(response, sw);
+            xmlResponse = sw.toString();
+
+            return response;
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //logging event
+            Instant finish = Instant.now();
+            long duration = Duration.between(start, finish).toMillis();
+            logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), duration, xmlRequest, xmlResponse);
+        }
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "pokemonAbilitiesRequest")
     @ResponsePayload
     public PokemonAbilitiesResponse getPokemonAbilities(@RequestPayload PokemonAbilitiesRequest request) {
-        logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), "getPokemonAbilities");
+        Instant start = Instant.now();
 
-        return pokemonService.getPokemonAbilities(request.getName());
+        try {
+            //generation of xml with request to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(request.getClass());
+            xmlContext();
+            jaxbMarshaller.marshal(request, sw);
+            xmlRequest = sw.toString();
+            xmlRequest = xmlRequest.replace("\n", "");
+
+            PokemonAbilitiesResponse response = pokemonService.getPokemonAbilities(request.getName());
+
+            //generation of xml with response to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(response.getClass());
+            jaxbMarshaller = contextObj.createMarshaller();
+            jaxbMarshaller.marshal(response, sw);
+            xmlResponse = sw.toString();
+
+            return response;
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //logging event
+            Instant finish = Instant.now();
+            long duration = Duration.between(start, finish).toMillis();
+            logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), duration, xmlRequest, xmlResponse);
+        }
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "pokemonHeldItemsRequest")
     @ResponsePayload
     public PokemonHeldItemsResponse getPokemonHeldItems(@RequestPayload PokemonHeldItemsRequest request) {
-        logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), "getPokemonHeldItems");
+        Instant start = Instant.now();
 
-        return pokemonService.getPokemonHeldItems(request.getName());
+        try {
+            //generation of xml with request to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(request.getClass());
+            xmlContext();
+            jaxbMarshaller.marshal(request, sw);
+            xmlRequest = sw.toString();
+            xmlRequest = xmlRequest.replace("\n", "");
+
+            PokemonHeldItemsResponse response = pokemonService.getPokemonHeldItems(request.getName());
+
+            //generation of xml with response to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(response.getClass());
+            jaxbMarshaller = contextObj.createMarshaller();
+            jaxbMarshaller.marshal(response, sw);
+            xmlResponse = sw.toString();
+
+            return response;
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //logging event
+            Instant finish = Instant.now();
+            long duration = Duration.between(start, finish).toMillis();
+            logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), duration, xmlRequest, xmlResponse);
+        }
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "pokemonLocationAreaEncountersRequest")
     @ResponsePayload
     public PokemonLocationAreaEncountersResponse getPokemonLocationAreaEncounters(@RequestPayload PokemonLocationAreaEncountersRequest request) {
-        logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), "getPokemonLocationAreaEncounters");
+        Instant start = Instant.now();
 
-        return pokemonService.getPokemonLocationAreaEncounters(request.getName());
-    }
+        try {
+            //generation of xml with request to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(request.getClass());
+            xmlContext();
+            jaxbMarshaller.marshal(request, sw);
+            xmlRequest = sw.toString();
+            xmlRequest = xmlRequest.replace("\n", "");
 
-    private void logEvent(String ipAddress, LocalDateTime dateTime, String methodName) {
-        eventRepository.save(new Event(
-                ipAddress,
-                dateTime,
-                methodName
-        ));
+            PokemonLocationAreaEncountersResponse response = pokemonService.getPokemonLocationAreaEncounters(request.getName());
+
+            //generation of xml with response to log it
+            sw = new StringWriter();
+            contextObj = JAXBContext.newInstance(response.getClass());
+            jaxbMarshaller = contextObj.createMarshaller();
+            jaxbMarshaller.marshal(response, sw);
+            xmlResponse = sw.toString();
+
+            return response;
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //logging event
+            Instant finish = Instant.now();
+            long duration = Duration.between(start, finish).toMillis();
+            logEvent(httpServletRequest.getRemoteAddr(), LocalDateTime.now(), duration, xmlRequest, xmlResponse);
+        }
     }
 }
